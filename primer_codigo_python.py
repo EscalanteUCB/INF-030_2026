07c8d76d1cec2d3f0abb46815f6461b3a9148195
EDDY ESCALANTE USTARIZ <eescalante@ucb.edu.bo>
2026-01-08 12:25:00 -0400

    clase de programacion

---
+++ primer_codigo_python.py
@@ -0,0 +1,19 @@
+print("Hola, Mundo!")
+
+x = 5
+y = 10
+resultado = x + y
+print("Hola, Alex el resultado es: ")
+print(resultado)
+
+print("-----------------------------")
+
+print("Calcular el area de un rectangulo")
+base = 10.25
+altura = 57.9
+area_rectangulo = base * altura
+print(area_rectangulo)
+
+print("-----------------------------")
+
+print(7*8)
